d1476bbd7a7e8535c9b30d583562286630728ea8
John Doe <johndoe@example.com>
2026-02-02 15:09:30 +0700

    A

--- src/app/example/components/display-tel/display-tel.ts
+++ src/app/example/components/display-tel/display-tel.ts
@@ -1,9 +1,10 @@
+import { DecimalPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input, numberAttribute } from '@angular/core';
 import { Contact } from '../../types';
 
 @Component({
   selector: 'app-display-tel',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './display-tel.html',
   styleUrl: './display-tel.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
